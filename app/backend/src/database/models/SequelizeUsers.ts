a4f7ae5b7500edbc04cec74fcdd65dba5bba1ccc
Lívio Lopes <antonio.lvio760@gmail.com>
2023-08-29 14:58:13 -0300

    feat(backend/database): create SequelizeUsers, the model User

---
+++ app/backend/src/database/models/SequelizeUsers.ts
@@ -0,0 +1,34 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  declare id: CreationOptional<number>;
+  declare userName: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  userName: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING },
+  email: { type: DataTypes.STRING },
+  password: { type: DataTypes.STRING },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+  underscored: true,
+});
