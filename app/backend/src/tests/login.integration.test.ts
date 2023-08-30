15cd5bc945dcb9a93891c77045eeee0185f56777
Lívio Lopes <antonio.lvio760@gmail.com>
2023-08-29 21:31:45 -0300

    test(backend/integration): create test of integration on endpoint POST /login

--- app/backend/src/tests/login.integration.test.ts
+++ app/backend/src/tests/login.integration.test.ts
@@ -1,22 +1,23 @@
-import MockTeams from "./mocks/MockTeams";
 import * as chai from "chai";
 import * as sinon from "sinon";
 // @ts-ignore
 import chaiHttp = require('chai-http');
 import { app } from "../app";
 import MockLogin from "./mocks/MockLogin";
+import SequelizeUser from "../database/models/SequelizeUsers";
 
 chai.use(chaiHttp);
 
 const BAD_REQUEST = 400;
 const OK = 200;
+const UNAUTHORIZED = 401;
 const ALL_FIELDS_REQUIRED = { "message": "All fields must be filled" }
-
+const INVALID_FIELDS = { message: 'Invalid email or password' };
 const { expect } = chai;
 
 
-describe('Test integration Login', () => {
-  afterEach(sinon.restore);
+describe.only('Test integration Login', () => {
+  beforeEach(sinon.restore);
   it('should return 400 when email is not provided', async () => {
     //arrange
     const httpBody = MockLogin.loginEmailRequired;
@@ -28,20 +29,61 @@ describe('Test integration Login', () => {
   })
   it('should return 400 when password is not provided', async () => {
     //arrange
-    const httpBody = MockLogin.loginPasswordInvalid;
+    const httpBody = MockLogin.loginPasswordRequired;
     //act
     const response = await chai.request(app).post('/login').send(httpBody);
     //assert
     expect(response.status).to.be.equal(BAD_REQUEST);
     expect(response.body).to.be.deep.equal(ALL_FIELDS_REQUIRED);
   })
+  it('should return 401 when email is invalid', async () => {
+    //arrange
+    const httpBody = MockLogin.loginEmailInvalid;
+    //act
+    const response = await chai.request(app).post('/login').send(httpBody);
+    //assert
+    expect(response.status).to.be.equal(UNAUTHORIZED);
+    expect(response.body).to.be.deep.equal(INVALID_FIELDS);
+  })
+  it('should return 401 when password is invalid', async () => {
+    //arrange
+    const httpBody = MockLogin.loginPasswordInvalid;
+    //act
+    const response = await chai.request(app).post('/login').send(httpBody);
+    //assert
+    expect(response.status).to.be.equal(UNAUTHORIZED);
+    expect(response.body).to.be.deep.equal(INVALID_FIELDS);
+  })
+  it('should return 401 when password is not found', async () => {
+    //arrange
+    const userModel = SequelizeUser.build(MockLogin.userLogged)
+    sinon.stub(SequelizeUser, 'findOne').resolves(userModel)
+    const httpBody = MockLogin.loginPasswordNotFound;
+    //act
+    const response = await chai.request(app).post('/login').send(httpBody);
+    //assert
+    expect(response.status).to.be.equal(UNAUTHORIZED);
+    expect(response.body).to.be.deep.equal(INVALID_FIELDS);
+  })
   it('should return token when email e password is valid', async () => {
     //arrange
+    const userModel = SequelizeUser.build(MockLogin.userLogged)
+    sinon.stub(SequelizeUser, 'findOne').resolves(userModel)
     const httpBody = MockLogin.loginValid;
     //act
     const response = await chai.request(app).post('/login').send(httpBody);
     //assert
     expect(response.status).to.be.equal(OK);
     expect(response.body).to.have.property('token');
+  })
+  it('should return 401 when email is not found', async () => {
+    //arrange
+    sinon.stub(SequelizeUser, 'findOne').resolves(null)
+    const httpBody = MockLogin.loginEmailNotFound;
+    //act
+    const response = await chai.request(app).post('/login').send(httpBody);
+    //assert
+    expect(response.status).to.be.equal(UNAUTHORIZED);
+    expect(response.body).to.be.deep.equal(INVALID_FIELDS);
   })
 })
--- app/backend/src/tests/mocks/MockLogin.ts
+++ app/backend/src/tests/mocks/MockLogin.ts
@@ -1,16 +1,30 @@
-import { password } from "../../database/config/database"
-
-const validEmail = 'bode@espiatorio.com'
-const validPassword = '123456'
+const validEmail = 'user@user.com'
+const validPassword = 'secret_user'
 
 const loginEmailRequired = { email: '', password: validPassword }
-const loginPasswordInvalid = { email: validEmail, password: '' }
+const loginPasswordRequired = { email: validEmail, password: '' }
 const loginValid = { email: validEmail, password: validPassword }
+const loginEmailInvalid = { email: 'invalid.bode.com', password: validPassword }
+const loginPasswordInvalid = { email: validEmail, password: '123' }
+const loginEmailNotFound = { email: 'notfound@batata.com', password: validPassword }
+const loginPasswordNotFound = { email: validEmail, password: 'notfound' }
 
+
+const userLogged = {
+  id: 2,
+  username: 'User',
+  role: 'user',
+  email: 'user@user.com',
+  password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',
+}
 
 export default {
   loginEmailRequired,
-
+  loginPasswordRequired,
+  loginEmailNotFound,
+  loginEmailInvalid,
   loginPasswordInvalid,
-  loginValid
+  loginPasswordNotFound,
+  loginValid,
+  userLogged
 }
